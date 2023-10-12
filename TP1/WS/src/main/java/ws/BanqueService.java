c19bd424ea8d591292c62715f059abc9e00eb35a
Jad Khafife <khafifejad@gmail.com>
2023-10-12 22:05:02 +0100

    First Activity

---
+++ TP1/WS/src/main/java/ws/BanqueService.java
@@ -0,0 +1,29 @@
+package ws;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+import jakarta.jws.WebService;
+
+import java.util.Date;
+import java.util.List;
+@WebService(serviceName = "BanqueWS")
+public class BanqueService {
+    @WebMethod(operationName = "conversionMTN")
+    public double conversion(@WebParam(name = "montant") double mtn) {
+        return mtn * 11;
+    }
+
+    @WebMethod
+    public Compte getCompte(@WebParam(name = "code")int code) {
+        return new Compte(code, Math.random() * 9000, new Date());
+    }
+
+    @WebMethod
+    public List<Compte> listComptes(){
+        return List.of(
+                new Compte(1, Math.random() * 9000, new Date()),
+                new Compte(2, Math.random() * 9000, new Date()),
+                new Compte(3, Math.random() * 9000, new Date())
+        );
+    }
+}
