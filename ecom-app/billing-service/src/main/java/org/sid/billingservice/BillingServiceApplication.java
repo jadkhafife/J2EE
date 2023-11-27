5826a6412fa8644eb9e7f2beb2453a90c9a2c6b1
jadkhafife <khafifejad@gmail.com>
2023-11-27 23:12:01 +0100

    FeignClient

--- ecom-app/billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
+++ ecom-app/billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
@@ -1,13 +1,59 @@
 package org.sid.billingservice;
 
+import org.sid.billingservice.entities.Bill;
+import org.sid.billingservice.entities.ProductItem;
+import org.sid.billingservice.model.Customer;
+import org.sid.billingservice.model.Product;
+import org.sid.billingservice.repositories.BillRepository;
+import org.sid.billingservice.repositories.ProductItemRepository;
+import org.sid.billingservice.services.CustomerRestClient;
+import org.sid.billingservice.services.ProductRestClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BillingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BillingServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner start(BillRepository billRepository,
+							ProductItemRepository productItemRepository,
+							CustomerRestClient customerRestClient,
+				ProductRestClient productRestClient) {
+		return args -> {
+			Collection<Product> products = productRestClient.allProducts().getContent();
+			Long customerId = 1L;
+			Customer customer = customerRestClient.findCustomerById(customerId);
+
+			if (customer == null) throw new RuntimeException("Customer not found");
+
+			Bill bill = new Bill();
+			bill.setBillingDate(new Date());
+			bill.setCustomerId(customerId);
+			Bill saved = billRepository.save(bill);
+
+			products.forEach(p -> {
+				ProductItem productItem = new ProductItem();
+				productItem.setBill(saved);
+				productItem.setProductId(p.getId());
+				productItem.setPrice(p.getPrice());
+				productItem.setQuantity(1 + new Random().nextInt(10));
+				productItem.setDiscount(Math.random());
+				productItemRepository.save(productItem);
+			});
+
+		};
+	}
 }
