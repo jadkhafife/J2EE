34720364e5ed6166b31e72cecb6564661753f2b1
jadkhafife <khafifejad@gmail.com>
2023-12-02 22:31:13 +0100

    New Components and Routes

--- TP4_5/emsi-app/src/app/app.routes.ts
+++ TP4_5/emsi-app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ProductsComponent} from "./products/products.component";
+import {NewProductComponent} from "./new-product/new-product.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  {path: 'products', component: ProductsComponent},
+  {path: 'newProduct', component: NewProductComponent}
+  // { path: 'about', component: AboutComponent },
+  // { path: 'contact', component: ContactComponent },
+  // { path: 'contactus', redirectTo: 'contact' },
+  // { path: '**', component: PageNotFoundComponent }
+];
