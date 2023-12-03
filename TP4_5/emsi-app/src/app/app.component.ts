6b007f03cb32ddf188ea7359992b76440a931fd6
jadkhafife <khafifejad@gmail.com>
2023-12-03 17:48:23 +0100

    Product Table added

--- TP4_5/emsi-app/src/app/app.component.ts
+++ TP4_5/emsi-app/src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'emsi-app';
+
+  actions : Array<any> = [
+    {title: 'Home', "route": '/home', icon: 'house'},
+    {title: 'Products', "route": '/products', icon: 'list'},
+    {title: 'New Product', "route": '/newProduct', icon: 'plus'}
+  ];
+
+  currentAction : any;
+
+  setCurrentAction(action: any) {
+    this.currentAction = action;
+  }
 }
--- TP4_5/emsi-app/src/app/products/products.component.ts
+++ TP4_5/emsi-app/src/app/products/products.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  products: Array<any> = [];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Array<any>>('http://localhost:8089/products')
+      .subscribe({
+        next : data => {this.products = data},
+        error : error => {console.log(error)}
+      });
+  }
+
+
+
+  handleCheckProduct(product: any) {
+    this.http.patch<any>(`http://localhost:8089/products/${product.id}`,
+      {checked: !product.checked})
+      .subscribe({
+        next : updated => {
+          product.checked = updated.checked;
+        },
+        error : error => {console.log(error)}
+      });
+
+  }
+
 
 }
