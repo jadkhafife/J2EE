5b9755ed3de34951402edee61db6bb68a96ccd23
jadkhafife <khafifejad@gmail.com>
2023-12-03 19:17:50 +0100

    Added Interceptor To communicate with the DB using HTTPClient

--- TP4_5/emsi-app/src/app/app.config.ts
+++ TP4_5/emsi-app/src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {HttpClientModule, provideHttpClient, withInterceptors} from "@angular/common/http";
+import {productInterceptor} from "./product.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),HttpClientModule, provideHttpClient(withInterceptors([productInterceptor]))]
 };
--- TP4_5/emsi-app/src/app/products/products.component.ts
+++ TP4_5/emsi-app/src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {NgForOf} from "@angular/common";
 import {HttpClient} from "@angular/common/http";
+import {ProductService} from "../services/product.service";
 
 @Component({
   selector: 'app-products',
@@ -13,27 +14,43 @@ import {HttpClient} from "@angular/common/http";
 })
 export class ProductsComponent implements OnInit{
   products: Array<any> = [];
-  constructor(private http: HttpClient) {}
+
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.http.get<Array<any>>('http://localhost:8089/products')
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productService.getProducts()
       .subscribe({
         next : data => {this.products = data},
         error : error => {console.log(error)}
       });
+    // this.http.get<Array<any>>('http://localhost:8089/products')
+    //   .subscribe({
+    //     next : data => {this.products = data},
+    //     error : error => {console.log(error)}
+    //   });
   }
 
 
-
   handleCheckProduct(product: any) {
-    this.http.patch<any>(`http://localhost:8089/products/${product.id}`,
-      {checked: !product.checked})
+    this.productService.checkProduct(product)
       .subscribe({
         next : updated => {
           product.checked = updated.checked;
         },
         error : error => {console.log(error)}
       });
+    // this.http.patch<any>(`http://localhost:8089/products/${product.id}`,
+    //   {checked: !product.checked})
+    //   .subscribe({
+    //     next : updated => {
+    //       product.checked = updated.checked;
+    //     },
+    //     error : error => {console.log(error)}
+    //   });
 
   }
 
