a0a0ca34f6deeec46a34527af69ddc50a3dea1a4
jadkhafife <khafifejad@gmail.com>
2023-12-26 22:54:14 +0100

    cors config added and Customers page updated

--- DevOps/bank-account-ms-emsi/angular-front-end/src/app/app.routes.ts
+++ DevOps/bank-account-ms-emsi/angular-front-end/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {CustomersComponent} from "./customers/customers.component";
+import {AccountsComponent} from "./accounts/accounts.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: "customers", component: CustomersComponent},
+  {path: "accounts", component: AccountsComponent},
+];
--- DevOps/bank-account-ms-emsi/angular-front-end/src/app/customers/customers.component.ts
+++ DevOps/bank-account-ms-emsi/angular-front-end/src/app/customers/customers.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf,
+    HttpClientModule
+  ],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css'
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit{
+  customers: any;
+  constructor(private http:HttpClient) { }
+  ngOnInit(): void {
+    this.http.get('http://localhost:8888/CUSTOMER-SERVICE/customers')
+      .subscribe({
+        next: data =>{
+          this.customers = data;
+        },
+        error: error => {
+          console.error('There was an error!', error);
+        }
+      })
+  }
 
 }
