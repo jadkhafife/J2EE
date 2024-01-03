36bcc9f679721c1ea30bff71048a476b30101ebb
jadkhafife <khafifejad@gmail.com>
2024-01-03 16:53:56 +0100

    Topic R4 Issue fixed

--- demo-springcloud-streams-kafka/src/main/java/org/sid/demospringcloudstreamskafka/services/PageEventService.java
+++ demo-springcloud-streams-kafka/src/main/java/org/sid/demospringcloudstreamskafka/services/PageEventService.java
@@ -10,6 +10,7 @@ import org.sid.demospringcloudstreamskafka.entities.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
 import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
@@ -20,47 +21,41 @@ import java.util.function.Supplier;
 @Service
 public class PageEventService {
     @Bean
-    public Consumer<PageEvent> pageEventConsumer(){
+    public Consumer<PageEvent> PageEventConsumer() {
         return (input) -> {
-            System.out.println("******************");
+            System.out.println("***********");
             System.out.println(input.toString());
-            System.out.println("******************");
+            System.out.println("***********");
         };
     }
-
     @Bean
-    public Supplier<PageEvent> pageEventSupplier(){
-        return () -> {
-            PageEvent pageEvent =  new PageEvent(
-                    Math.random()>0.5?"page1":"page2",
-                    Math.random()>0.5?"user1":"user2",
-                    new Date(),
-                    (long) new Random().nextInt(9000));
-            return pageEvent;
-        };
+    public Supplier<PageEvent> PageEventSupplier() {
+        return ()->  new PageEvent(Math.random()>0.5?"P1":"P2",
+                Math.random()>0.5?"U1":"U2",
+                new Date(),
+                new Random().nextInt(9000));
     }
-
     @Bean
-    public Function<PageEvent, PageEvent> pageEventFunction(){
-        return (input) -> {
-            input.setName("L"+input.getName().length());
-            input.setUser("UUUUUUUUUUU");
+    public Function<PageEvent,PageEvent> PageEventFunction(){
+        return  (input)->{
+            input.setName("Page Event");
+            input.setUser("JAD");
             return input;
+
+        };
+    }
+    @Bean
+    public  Function<KStream<String,PageEvent>,KStream<String,Long>> KstreamFunction(){
+        return  (input)->{
+            return input
+                    .filter((k,v)->v.getDuration()>100)
+                    .map((k,v)->new KeyValue<>(v.getName(),0L))
+                    .groupBy((k,v)->k,Grouped.with(Serdes.String(),Serdes.Long()))
+                    .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
+                    .count(Materialized.as("page-count"))
+                    .toStream()
+                    .map((k,v)->new KeyValue<>("=>"+k.window().startTime()+k.window().endTime()+k.key(),v));
         };
     }
 
-    @Bean
-    public Function<KStream<String, PageEvent>, KStream<String, Long>> kStreamFunction(){
-        return (input) -> {
-            return input
-                    .filter((k,v) -> v.getDuration()>100)
-                    .mapValues((k,v) -> new KeyValue<>(v.getName(), 0L))
-                    .groupBy((k,v) -> k)
-                    .windowedBy(TimeWindows.of(Duration.ofMillis(5000)))
-                    .count(Materialized.as("pageCount"))
-                    .toStream()
-                    .map((k,v) -> new KeyValue<>("=>"+k.window().startTime()+k.window().endTime(), v));
-//                    .groupByKey(Grouped.keySerde(Serdes.String()))
-        };
-    }
 }
--- demo-springcloud-streams-kafka/src/main/java/org/sid/demospringcloudstreamskafka/web/PageEventRestController.java
+++ demo-springcloud-streams-kafka/src/main/java/org/sid/demospringcloudstreamskafka/web/PageEventRestController.java
@@ -5,6 +5,7 @@ import org.apache.kafka.streams.kstream.Windowed;
 import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.QueryableStoreTypes;
 import org.apache.kafka.streams.state.ReadOnlyWindowStore;
+import org.apache.kafka.streams.state.WindowStoreIterator;
 import org.sid.demospringcloudstreamskafka.entities.PageEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.awt.*;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
@@ -24,35 +26,33 @@ import java.util.Random;
 
 @RestController
 public class PageEventRestController {
-
-    private final StreamBridge streamBridge;
-    private final InteractiveQueryService interactiveQueryService;
-
-    public PageEventRestController(StreamBridge streamBridge, InteractiveQueryService interactiveQueryService) {
-        this.streamBridge = streamBridge;
-        this.interactiveQueryService = interactiveQueryService;
-    }
+    @Autowired
+    private StreamBridge streamBridge;
+    @Autowired
+    private InteractiveQueryService interactiveQueryService;
 
     @GetMapping("/publish/{topic}/{name}")
-    public PageEvent publish(@PathVariable String topic, @PathVariable String name){
-        PageEvent pageEvent = new PageEvent(name, Math.random()>0.5?"user1":"user2", new Date(), (long) new Random().nextInt(9000));
+    public PageEvent publish(@PathVariable String topic, @PathVariable String name) {
+        PageEvent pageEvent = new PageEvent(name, Math.random() > 0.5 ? "U1" : "U2", new Date(), new Random().nextInt(9000));
         streamBridge.send(topic, pageEvent);
         return pageEvent;
     }
 
     @GetMapping(path = "/analytics", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Map<String, Long>> analytics(){
+    public Flux<Map<String, Long>> analytics() {
         return Flux.interval(Duration.ofSeconds(1))
-                .map(sequence-> {
+                .map(sequence -> {
                     Map<String, Long> stringLongMap = new HashMap<>();
-                    ReadOnlyWindowStore<String, Long> stats = interactiveQueryService.getQueryableStore("pageCount", QueryableStoreTypes.windowStore());
+                    ReadOnlyWindowStore<String, Long> windowStore = interactiveQueryService.getQueryableStore("page-count", QueryableStoreTypes.windowStore());
                     Instant now = Instant.now();
                     Instant from = now.minusMillis(5000);
-                    KeyValueIterator<Windowed<String>, Long> fetchAll = stats.fetchAll(from, now);
-                    while (fetchAll.hasNext()) {
-                        KeyValue<Windowed<String>, Long> next = fetchAll.next();
+                    KeyValueIterator<Windowed<String>, Long> fetchall = windowStore.all();
+                    while (fetchall.hasNext()) {
+                        KeyValue<Windowed<String>, Long> next = fetchall.next();
                         stringLongMap.put(next.key.key(), next.value);
                     }
+
+
                     return stringLongMap;
                 }).share();
     }
