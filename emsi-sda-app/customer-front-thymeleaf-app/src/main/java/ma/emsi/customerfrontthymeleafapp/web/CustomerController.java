ef7a3949f9e4810a7072f4268d91338269185166
jadkhafife <khafifejad@gmail.com>
2024-01-14 23:02:10 +0100

    Auth Configuration with github and KeyCloak

--- emsi-sda-app/customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/web/CustomerController.java
+++ emsi-sda-app/customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/web/CustomerController.java
@@ -2,23 +2,31 @@ package ma.emsi.customerfrontthymeleafapp.web;
 
 import ma.emsi.customerfrontthymeleafapp.entities.Customer;
 import ma.emsi.customerfrontthymeleafapp.repositories.CustomerRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class CustomerController {
     private final CustomerRepository customerRepository;
+    private ClientRegistrationRepository clientRegistrationRepository;
 
-    public CustomerController(CustomerRepository customerRepository) {
+    public CustomerController(CustomerRepository customerRepository, ClientRegistrationRepository clientRegistrationRepository) {
         this.customerRepository = customerRepository;
+        this.clientRegistrationRepository = clientRegistrationRepository;
     }
 
     @GetMapping("/customers")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String customers(Model model){
         List<Customer> customers = customerRepository.findAll();
         model.addAttribute("customers", customers);
@@ -39,5 +47,23 @@ public class CustomerController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/oauth2/login")
+    public String oauth2Login(Model model){
+        String authorizationRequestBaseUri = "oauth2/authorization";
+        Map<String, String> oauth2AuthenticationUrls = new HashMap();
+        Iterable<ClientRegistration> clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;;
+        clientRegistrations.forEach(registration ->{
+            oauth2AuthenticationUrls.put(registration.getClientName(),
+                    authorizationRequestBaseUri + "/" + registration.getRegistrationId());
+        });
+        model.addAttribute("urls", oauth2AuthenticationUrls);
+        return "oauth2Login";
+    }
+
+    @GetMapping("/unauthorized")
+    public String unauthorized(){
+        return "unauthorized";
     }
 }
